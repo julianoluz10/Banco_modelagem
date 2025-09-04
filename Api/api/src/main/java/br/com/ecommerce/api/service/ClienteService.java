8029df9267600cded1804ac3975afc960601b278
A444le <alexandrinymiguel@gmail.com>
2025-09-03 21:51:57 -0300

    ApiBancoEcomerce

---
+++ Api/api/src/main/java/br/com/ecommerce/api/service/ClienteService.java
@@ -0,0 +1,25 @@
+package br.com.ecommerce.api.service;
+
+import br.com.ecommerce.api.model.Cliente;
+import br.com.ecommerce.api.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+    //Injecao de Dependencia
+    //Falar que Service depende de  alguem
+    // final constante
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository repo) {
+        clienteRepository = repo;
+    }
+
+    //Listar todos os clientes
+    public List<Cliente> listarTodos(){
+        return clienteRepository.findAll();
+    }
+
+}
